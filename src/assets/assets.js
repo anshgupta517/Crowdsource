afa9b97238b06fe051e0c350a483423bf65368c0
Naman Jain <162738854+NamanjainAkt@users.noreply.github.com>
2025-09-18 18:52:58 +0530

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,5 +4,6 @@ import overloadedDustbin from './overloadedDustbin.jpg';
 import pothHole from  './pothHole.jpg';
 import waterLeakage from './waterLeakage.jpg';
 import qrcode from './qrcode.png';
+import sureshWorker from './sureshWorker.png';
 
-export default {brokenLamp, brokenPark, overloadedDustbin, pothHole, waterLeakage,qrcode};
+export default {brokenLamp, brokenPark, overloadedDustbin, pothHole, waterLeakage,qrcode,sureshWorker};
