f7fdf66b8d1919e88e1f5c837ff94d0ff577ba3e
Naman Jain <162738854+NamanjainAkt@users.noreply.github.com>
2025-09-18 18:41:48 +0530

    import error fixed for deployment

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,17 +1,8 @@
-import brokenLamp from "./brokenLamp.jpg"
-import brokenPark from "./brokenPark.jpg"
-import overloadedDustbin from "./overloadedDustbin.jpg"
-import pothHole from "./pothHole.jpg"
-import waterLeakage from "./waterLeakage.jpg"
-import qrcode from "./qrcode.png"
-import sureshWorker from "./sureshWorker.png"
+import brokenLamp from './brokenLamp.jpg';
+import brokenPark from './brokenPark.jpg';
+import overloadedDustbin from './overloadedDustbin.jpg';
+import pothHole from  './pothHole.jpg';
+import waterLeakage from './waterLeakage.jpg';
+import qrcode from './qrcode.png';
 
-export default {
-    brokenLamp,
-    brokenPark,
-    overloadedDustbin,
-    pothHole,
-    waterLeakage,
-    qrcode,
-    sureshWorker
-}
+export default {brokenLamp, brokenPark, overloadedDustbin, pothHole, waterLeakage,qrcode};
